e38c726b7f4a32fa7aac3514fd6b3762692552d7
VMihovska <vili_mihovska@live.com>
2019-05-17 16:42:50 +0300

    validator tests

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -4,8 +4,39 @@ var expect = chai.expect;
 var validator = require('../lib/validator');
 
 describe('A Validator', function(){
-    it('will return error.nonpositive for not strictly positive numbers', function(){
-        // assert.deepEqual(validator(0), ['error.nonpositive']);
-        expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+    it('will return no errors for valid numbers', function(){
+        expect(validator(7)).to.be.empty;
+    });
+
+    describe('will include error.nonpositive for not strictly positive numbers: ', function(){
+        it('like 0', function(){
+            // assert.deepEqual(validator(0), ['error.nonpositive']);
+            expect(validator(0)).to.include('error.nonpositive');
+        });
+    
+        it('like -2', function(){
+            expect(validator(-2)).to.include('error.nonpositive');
+        });
+    });
+
+    describe('will include error.three for divisible by 3 numbers: ', function(){
+        it('like 3', function(){
+            expect(validator(3)).to.include('error.three');
+        });
+
+        it('like 15', function(){
+            expect(validator(15)).to.include('error.three');
+        });
+    });
+
+    describe('will include error.five for divisible by 5 numbers: ', function(){
+        it('like 5', function(){
+            expect(validator(5)).to.include('error.five');
+        });
+
+        it('like 15', function(){
+            expect(validator(15)).to.include('error.five');
+        });
     });
 });
+
