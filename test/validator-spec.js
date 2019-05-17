ddbf3925b942b77c0d3a4fd895f8d8280b6fd55c
VMihovska <vili_mihovska@live.com>
2019-05-17 17:03:58 +0300

    extracting test cases to a function

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -3,40 +3,32 @@ var chai = require('chai');
 var expect = chai.expect;
 var validator = require('../lib/validator');
 
+function expected_to_include_error_when_invalid(number, error){
+    it('like ' + number, function(){
+        expect(validator(number)).to.include(error);
+    });
+}
+
 describe('A Validator', function(){
     it('will return no errors for valid numbers', function(){
         expect(validator(7)).to.be.empty;
     });
 
     describe('will include error.nonpositive for not strictly positive numbers: ', function(){
-        it('like 0', function(){
-            // assert.deepEqual(validator(0), ['error.nonpositive']);
-            expect(validator(0)).to.include('error.nonpositive');
-        });
+        expected_to_include_error_when_invalid(0, 'error.nonpositive');
+        expected_to_include_error_when_invalid(-2, 'error.nonpositive');
     
-        it('like -2', function(){
-            expect(validator(-2)).to.include('error.nonpositive');
-        });
+        
     });
 
     describe('will include error.three for divisible by 3 numbers: ', function(){
-        it('like 3', function(){
-            expect(validator(3)).to.include('error.three');
-        });
-
-        it('like 15', function(){
-            expect(validator(15)).to.include('error.three');
-        });
+        expected_to_include_error_when_invalid(3, 'error.three');
+        expected_to_include_error_when_invalid(15, 'error.three');
     });
 
     describe('will include error.five for divisible by 5 numbers: ', function(){
-        it('like 5', function(){
-            expect(validator(5)).to.include('error.five');
-        });
-
-        it('like 15', function(){
-            expect(validator(15)).to.include('error.five');
-        });
+        expected_to_include_error_when_invalid(5, 'error.five');
+        expected_to_include_error_when_invalid(15, 'error.five');
     });
 });
 
