22cd323e41e6f4148a84e5c34495c4ba4bacc8c5
VMihovska <vili_mihovska@live.com>
2019-05-20 16:35:19 +0300

    setup/arrange tests

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,34 +1,51 @@
-// var assert = require('assert');
 var chai = require('chai');
 var expect = chai.expect;
-var validator = require('../lib/validator');
+var validator_with = require('../lib/validator');
+var non_positive_validation_rule = require('../lib/validator/rules/non_positive');
+var non_divisible_validation_rule = require('../lib/validator/rules/non_divisible');
 
-function expected_to_include_error_when_invalid(number, error){
-    it('like ' + number, function(){
-        expect(validator(number)).to.include(error);
-    });
-}
 
 describe('A Validator', function(){
+    var validator;
+    beforeEach(function(){
+        validator = validator_with([
+            non_positive_validation_rule,
+            non_divisible_validation_rule(3, 'error.three'),
+            non_divisible_validation_rule(5, 'error.five')
+        ]);
+    });
     it('will return no errors for valid numbers', function(){
         expect(validator(7)).to.be.empty;
     });
 
     describe('will include error.nonpositive for not strictly positive numbers: ', function(){
-        expected_to_include_error_when_invalid(0, 'error.nonpositive');
-        expected_to_include_error_when_invalid(-2, 'error.nonpositive');
-    
-        
+        it('like 0', function(){
+            expect(validator(0)).to.include('error.nonpositive');
+        });
+
+        it('like -2', function(){
+            expect(validator(-2)).to.include('error.nonpositive');
+        });
     });
 
     describe('will include error.three for divisible by 3 numbers: ', function(){
-        expected_to_include_error_when_invalid(3, 'error.three');
-        expected_to_include_error_when_invalid(15, 'error.three');
+        it('like 3', function(){
+            expect(validator(3)).to.include('error.three');
+        });
+
+        it('like 15', function(){
+            expect(validator(15)).to.include('error.three');
+        });
     });
 
     describe('will include error.five for divisible by 5 numbers: ', function(){
-        expected_to_include_error_when_invalid(5, 'error.five');
-        expected_to_include_error_when_invalid(15, 'error.five');
+        it('like 5', function(){
+            expect(validator(5)).to.include('error.five');
+        });
+
+        it('like 15', function(){
+            expect(validator(15)).to.include('error.five');
+        });
     });
 });
 
